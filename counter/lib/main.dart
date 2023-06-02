baa24f75a3b0a17ff8f6841d0ebb86a98fa40224
th <taehee.k102@gmail.com>
2023-06-02 12:42:35 +0900

    widget lifecycle (stateful widgets #4.2 ~ #4.4)

--- counter/lib/main.dart
+++ counter/lib/main.dart
@@ -18,7 +18,14 @@ class App extends StatefulWidget {
 
 // State : 위젯의 데이터, 상태를 저장
 class _AppState extends State<App> {
-  int counter = 0;
+  bool showTitle = true;
+  List<int> numbers = [];
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
 
   void onClicked() {
     // State 클래스에 상태가 변경됨을 알리는 함수
@@ -26,40 +33,98 @@ class _AppState extends State<App> {
     setState(() {
       // 데이터를 변경하는 코드를 setState 안에 넣을 필요는 없으나
       // 넣는게 가독성이 좋음.
-      counter = counter + 1;
+
+      numbers.add(numbers.length + 1);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // theme : 어플리케이션을 위한 스타일시트 지정
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
       home: Scaffold(
           backgroundColor: const Color(0xfff4eddb),
           body: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Text(
-                  'Click Count',
-                  style: TextStyle(
-                    fontSize: 30,
-                  ),
-                ),
-                Text(
-                  '$counter',
-                  style: const TextStyle(
-                    fontSize: 30,
-                  ),
-                ),
+                showTitle
+                    ? const MyTitle()
+                    : const Text(
+                        ':)',
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                      ),
+                for (var n in numbers) Text('$n'),
                 IconButton(
                   iconSize: 40,
                   color: Colors.deepOrange[700],
                   onPressed: onClicked,
                   icon: const Icon(Icons.add_circle),
-                )
+                ),
+                IconButton(
+                  iconSize: 40,
+                  color: Colors.grey[700],
+                  onPressed: toggleTitle,
+                  icon: const Icon(Icons.remove_red_eye),
+                ),
               ],
             ),
           )),
+    );
+  }
+}
+
+class MyTitle extends StatefulWidget {
+  const MyTitle({
+    super.key,
+  });
+
+  @override
+  State<MyTitle> createState() => _MyTitleState();
+}
+
+class _MyTitleState extends State<MyTitle> {
+  // 대부분의 경우 여기서 변수 초기화
+
+  @override
+  void initState() {
+    // initState
+    // 부모 요소를 통해 데이터를 초기화하는 경우 사용
+    // build 보다 먼저 호출되어야 함.
+    // 단 한번만 호출
+    super.initState();
+    print('init state!');
+  }
+
+  @override
+  void dispose() {
+    // dispose
+    // 위젯이 화면에서(위젯 트리에서) 제거되는 경우 호출됨.
+    super.dispose();
+    print('dispose!');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('build!');
+
+    // BuildContext : 상위 요소들에 대한 정보, 위젯의 위치 등을 담고있다.
+    // context를 통해 상위 MaterialApp에서 지정한 ThemeData에 접근
+    return Text(
+      'Click Count',
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge?.color,
+      ),
     );
   }
 }
